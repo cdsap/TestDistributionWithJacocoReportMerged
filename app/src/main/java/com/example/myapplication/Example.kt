a692f0cefe4d5bbc0ba9374df46f8617e1f32454
Inaki Villar <inaki.seri@gmail.com>
2023-05-09 13:10:36 +0200

    initial import

---
+++ app/src/main/java/com/example/myapplication/Example.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+class Example {
+
+    fun alo() : Int {
+        return 1
+    }
+
+    fun b(): Int {
+        return 2
+    }
+
+    fun c(): Int {
+        return 3
+    }
+}
---
+++ app/src/test/java/com/example/myapplication/ExampleUnit16Test.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnit16Test {
+    @Test
+    fun addition_isCorrect() {
+        val example = Example()
+
+        assertTrue(example.b() == 2)
+
+        assertEquals(4, 2 + 2)
+    }
+}
